91234dbe0deb6995770808ec8e11012cef08e446
Sarath Damaraju <sarathdamaraju@hotmail.com>
2020-04-24 14:33:05 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-const withCSS = require('@zeit/next-css')
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/
-})
-module.exports = withCSS(withMDX());
+module.exports = {
+  webpack: (cfg) => {
+      cfg.module.rules.push(
+          {
+              test: /\.md$/,
+              loader: 'frontmatter-markdown-loader',
+              options: { mode: ['react-component'] }
+          }
+      )
+      return cfg;
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,28 @@
-import React from 'react'
+import Head from "next/head"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/index.md';
 
-import Layout from '../components/Layout';
-import Journalslist from '../static/content/journals/Journalslist';
-
-const data = {
-  name: 'Krishna Sarath',
-  description: 'a JS dev — with No adjectives attached. Works primarily with web technologies. ',
+export default class Home extends Component {
+  render() {
+    let { title, cats } = attributes;
+    return (
+      <>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </>
+    )
+  }
 }
-
-const Index = () => (
-  <Layout heading="" title="KD :: Portfolio">
-
-    <section>
-      <div className="details">
-        <p>hi there 🖐️, i'm</p>
-        <h1>{data.name}</h1>
-        <p>{data.description}</p>
-      </div>
-
-      <div className="recent-blog">
-        <h4 className="page-subtitle">recent journal</h4>
-        <h3>{Journalslist[0].name}</h3>
-        <a href={Journalslist[0].link}>read now</a>
-      </div>
-    </section>
-
-    {/* Local Styles */}
-
-    <style jsx>{` 
-      section {
-        height: 570px;
-        display: -webkit-box;
-        display: -moz-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        -webkit-flex-flow: row wrap;
-      }
-      .details {
-        width: 50%
-      }
-      .details h1 {
-        font-family: 'la-sonnabula';
-        text-decoration: underline;
-        color: #CB734D;
-        text-transform: capitalize;
-      }
-      .recent-blog {
-        width: 572px;
-        min-height: 220px;
-        border: 4px solid #CB734D;
-        padding: 24px 32px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        overflow: hidden;
-      }
-      @media only screen and (max-width: 991px) {
-        .details {
-          width: 100%;
-        }
-        section {
-          height: 100vh;
-        }
-      }
-    `}</style>
-
-  </Layout>
-)
-
-
-export default Index;
